1962a0f268cbeeb6e7b42bbc31ea8bf35e7ab89d
sathukrish <sathusathushan0530@gmail.com>
2023-03-16 12:42:33 +0530

    adding x.html and y.html

--- server/html.js
+++ server/html.js
@@ -1,10 +1,19 @@
 const http = require ('http');
 const fs = require ('fs')
 const server = http.createServer ((req, res) =>{
-    fs.readFile('index.html', 'utf-8', function(err, data) {
+  if(req.url === './'){  fs.readFile('index.html', 'utf-8', function(err, data) {
         if (err) throw err;
-        res.end(data);
-    });
+        res.end(data)
+    });}else if (req.url === '/about'){
+        fs.readFile('x.html', 'utf-8', function(err, data) {
+            if (err) throw err;
+            res.end(data);
+    })}
+    else if (req.url === '/contact'){
+        fs.readFile('y.html', 'utf-8', function(err, data) {
+            if (err) throw err;
+            res.end(data);
+    })}
 });
 server.listen (3500, () => {
     console.log ('server running on port 3500')

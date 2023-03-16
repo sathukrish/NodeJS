88dd8e0c1517ac13ea39c4548e694e71158f9646
sathukrish <sathusathushan0530@gmail.com>
2023-03-16 14:09:10 +0530

    add symbol

--- server/html.js
+++ server/html.js
@@ -1,20 +1,25 @@
 const http = require ('http');
-const fs = require ('fs')
+const fs = require ('fs');
+
 const server = http.createServer ((req, res) =>{
-  if(req.url === './'){  fs.readFile('index.html', 'utf-8', function(err, data) {
+  if(req.url === '/'){  
+    fs.readFile('index.html', 'utf-8', function(err, data) {
         if (err) throw err;
         res.end(data)
-    });}else if (req.url === '/about'){
+    });
+} else if (req.url === '/about'){
         fs.readFile('x.html', 'utf-8', function(err, data) {
             if (err) throw err;
             res.end(data);
-    })}
-    else if (req.url === '/contact'){
+    })
+} else if (req.url === '/contact'){
         fs.readFile('y.html', 'utf-8', function(err, data) {
             if (err) throw err;
             res.end(data);
-    })}
+    })
+}
 });
-server.listen (3500, () => {
-    console.log ('server running on port 3500')
+
+server.listen (3000, '127.0.0.1', () => {
+    console.log ('server running on port 3000')
 });
